5eed6a5decc002499aabe2871131ee95ecaba477
Dương Đức Khải <greenjohnny135@gmail.com>
2024-04-29 11:15:14 +0700

    add conditional check in MyShop
    if user is logged in & has a shop: display shop
    if user is logged in & no shop: display seller form
    if user isn't logged in: display LogInRequired view

---
+++ src/components/sellerForm/SellerForm.jsx
@@ -0,0 +1,9 @@
+const SellerForm = (props) => {
+    return (<>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+        <h1 className="text-primary">This is a seller form</h1>
+      </div>
+    </>)
+}
+
+export default SellerForm;
--- src/views/shop/MyShop.jsx
+++ src/views/shop/MyShop.jsx
@@ -1,20 +1,42 @@
-import { lazy } from "react";
+import { lazy, useState } from "react";
+const LogInRequired = lazy(() => import("../pages/LogInRequired"));
 const Search = lazy(() => import("../../components/Search"));
-const ShopInfor  = lazy(() => import("../../components/shop/ShopInfor"));
-const ShopProduct  = lazy(() => import("../../components/shop/ShopProduct"));
+const ShopInfor = lazy(() => import("../../components/shop/ShopInfor"));
+const ShopProduct = lazy(() => import("../../components/shop/ShopProduct"));
+const SellerForm = lazy(() => import("../../components/sellerForm/SellerForm"));
 
-const MyShopView = () => {
-    return(
-        <div>
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <ShopInfor /> </div>
-        <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-        <ShopProduct />
-        <ShopProduct />
-        <ShopProduct />
-      </div>
-        </div>
-    )
+const MyShopView = (props) => {
+  const { userData, submitting, onSubmit } = props;
+  const [hasShop, setHasShop] = useState(false);
+
+  if (userData == null)
+    return (
+      <LogInRequired />
+    );
+
+  //fetch user's shop data
+
+  //if user has a shop, display it; otherwise, shows a form to apply for seller
+  return (
+    <div>
+      {!hasShop ? (
+        //apply for seller form
+        <SellerForm />
+      ) : (
+        //shop
+        <>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <ShopInfor />{" "}
+          </div>
+          <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+            <ShopProduct />
+            <ShopProduct />
+            <ShopProduct />
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default MyShopView;
